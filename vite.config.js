1bda792a56def0c1213778a77461e40e8311fe1c
Jannick Pedersen <jannickpedersen@live.dk>
2024-05-09 23:37:16 +0200

    initailzed the cli sentry for tracking for the free trials

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
-    org: "JannickDev",
+    org: "jannickdev",
     project: "javascript-react"
-  }),
-  ],
+  })],
 
   build: {
     sourcemap: true
